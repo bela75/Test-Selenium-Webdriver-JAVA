9e81348189b9f199c169eb8d3d8afe28fe9692e1
Belaid <bela.amb@gmail.com>
2020-06-04 19:16:19 +0200

    First commit

---
+++ src/main/java/simulateurpoleemploi/Tests/EstimationMonAllocationChomage.java
@@ -0,0 +1,27 @@
+package simulateurpoleemploi.Tests;
+
+
+import simulateurpoleemploi.pagespbjects.pageEstimerMesAllocations;
+
+public
+class EstimationMonAllocationChomage {
+    public static
+    void main ( String[] args ) throws InterruptedException {
+        pageEstimerMesAllocations browser = new pageEstimerMesAllocations ();
+
+        browser.URLSITEWEB ( browser.URL () );
+        browser.DateDeDaissance ( "23/07/1979" );
+        browser.SoumisAuxCotisations ();
+        browser.CategorieEmploi ( "Régime général" );
+        browser.DateDebut ( "03/09/2018" );
+        browser.DateFin ( "30/05/2020" );
+        browser.SalaireBrut ( "3100" );
+        browser.PeriodeContrat ( "Mensuel" );
+        browser.MotifFinContrat ( "Fin de contrat à durée déterminée" );
+        browser.IndemniteCongesPayes ( "0" );
+        browser.ClickAjouterButton ();
+        browser.ClickLancerEstimation ();
+        browser.dureeDindemnisation();
+
+    }
+}
---
+++ src/main/java/simulateurpoleemploi/pagespbjects/pageEstimerMesAllocations.java
@@ -0,0 +1,185 @@
+package simulateurpoleemploi.pagespbjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class
+pageEstimerMesAllocations {
+
+    protected final String url = "https://candidat.pole-emploi.fr/candidat/simucalcul/perteemploi";
+    protected final String datedenaissance = "#dateNaissance";
+    protected final String alsaceMoselle = "#nonCotisantAlsace";
+    protected final String categorieemploi = "#categorieEmploi";
+    protected final String dateDebutEmploi = "#dateDebutEmploi";
+    protected final String dateFinEmploi = "#dateFinEmploi";
+    protected final String salairebrut = "#salaireBrut";
+    protected final String periodecontrat = "#salaireBrutFrequence";
+    protected final String motiffincontrat = "#motifFinContrat";
+    protected final String indemnitecongespayes = "#indemnitesCongesPayes";
+    protected final String indemnitelicenciement = "#indemnitesLicenciement";
+    protected final String ajouterbutton = "#boutonAjouterEmploi";
+    protected final String lancerestimation = "#boutonValider";
+    protected final String buttonPOPAccepter = "#footer_tc_privacy_button";
+    protected final String MontantDroits = "#formulaire > div.bd.main-content > div:nth-child(3) > div > div.intro.refund > div > p:nth-child(3) > strong";
+    protected final String nombreJours = "#formulaire > div.bd.main-content > div:nth-child(3) > div > div:nth-child(3) > div > table > tbody > tr:nth-child(1) > td > strong";
+    private final String dureeIndemnisation ="#formulaire > div.bd.main-content > div:nth-child(3) > div > div.intro.refund > div > p:nth-child(1) > strong";
+    protected WebDriver driver;
+    protected WebElement element;
+
+
+    public
+    String URL () {
+        return url;
+    }
+
+
+    @Test
+    public
+    void URLSITEWEB ( String url ) {
+        System.setProperty ( "webdriver.chrome.driver" , "browser/ChromeDriver.81.0.4044.138" );
+        driver = new ChromeDriver ();
+        Dimension dimension = new Dimension ( 1280 , 1024 );
+        driver.manage ().window ().setSize ( dimension );
+        driver.get ( URL () );
+        driver.findElement ( By.cssSelector ( buttonPOPAccepter ) ).click ();
+    }
+
+    @Test
+    public
+    void DateDeDaissance ( String date ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( datedenaissance ) ).sendKeys ( date );
+    }
+    @Test
+    public
+    void SoumisAuxCotisations () throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( alsaceMoselle ) ).click ();
+    }
+
+    @Test
+    public
+    void CategorieEmploi ( String categorieEmploi ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( categorieemploi ) ).sendKeys ( categorieEmploi );
+
+    }
+
+    @Test
+    public
+    void DateDebut ( String datedebut ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( dateDebutEmploi ) ).sendKeys ( datedebut );
+    }
+
+    @Test
+    public
+    void DateFin ( String datefin ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( dateFinEmploi ) ).sendKeys ( datefin );
+    }
+
+    @Test
+    public
+    void SalaireBrut ( String salaire ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( salairebrut ) ).sendKeys ( salaire );
+    }
+
+    @Test
+    public
+    void MotifFinContrat ( String motif ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( motiffincontrat ) ).sendKeys ( motif );
+    }
+
+    @Test
+    public
+    void PeriodeContrat ( String periode ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( periodecontrat ) ).sendKeys ( periode );
+    }
+
+    @Test
+    public
+    void IndemniteCongesPayes ( String indemnite ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( indemnitecongespayes ) ).sendKeys ( indemnite );
+    }
+
+    @Test
+    public
+    void IndemniteLicenciement ( String indemnite ) throws InterruptedException {
+        Thread.sleep ( 1000 );
+        driver.findElement ( By.cssSelector ( indemnitelicenciement ) ).sendKeys ( "0" );
+    }
+
+    @Test
+    public
+    void ClickAjouterButton () throws InterruptedException {
+        element = driver.findElement ( By.cssSelector ( ajouterbutton ) );
+        Thread.sleep ( 5000 );
+        element.click ();
+    }
+    @Test
+    public
+    void ClickLancerEstimation () throws InterruptedException {
+        element = driver.findElement ( By.cssSelector ( lancerestimation ) );
+        Thread.sleep ( 5000 );
+        element.click ();
+
+    }
+
+    @Test
+    public
+    WebElement returnElement () {
+        try{
+            element = driver.findElement ( By.cssSelector ( nombreJours ) );
+        }
+        catch (Exception e){
+            System.out.println ("Exception dans la methode return element nombreJours : "+e);
+        }
+        return element;
+    }
+
+    @Test
+    public
+    void MontantGlobalDesDroits () {
+        element = returnElement ();
+        String text = element.getText ();
+        try {
+            System.out.println ("Message variable Text : "+text);
+            Assert.assertEquals ( text , "200" );
+        } catch (Exception e) {
+            System.out.println ( "Le nombre de jours est erronée : +" + e );
+        } finally {
+            //driver.close ();
+        }
+    }
+
+    @Test
+    public
+    WebElement returndureeDindemnisation () {
+        element = driver.findElement ( By.cssSelector ( dureeIndemnisation ) );
+        return element;
+    }
+
+    @Test
+    public void dureeDindemnisation(){
+        WebElement elementdureeDindemnisation = returndureeDindemnisation ();
+        String text = elementdureeDindemnisation.getText ();
+        try {
+            Assert.assertEquals ( text , "638 jours." );
+        } catch (AssertionError e) {
+            System.out.println ( "Durée d'indemnisation est erronée : +" + e );
+        } finally {
+            driver.close ();
+        }
+    }
+}
